60cf37f5470207b0bc0b19e52080c3c4310247a2
TypicalMonkey <pancerny677@interia.pl>
2024-06-09 12:24:41 +0200

    login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,9 @@ import { Navbar } from './components/Navbar'
 import { ShoppingCartProvider } from './context/ShoppingCartContext'
 import Payments from './pages/Payments'
 import { Warehouse } from "./pages/Warehouse";
+import Register from './pages/Register'
+import Login from './pages/Login';
+
 
 function App() {
 
@@ -18,6 +21,8 @@ function App() {
           <Route path='/' element={<Home />}></Route>
           <Route path='/payments' element={<Payments />}></Route>
           <Route path='/books' element={<Products />}></Route>
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
           <Route path="/warehouse" element={<Warehouse />} />
         </Routes>
       </Container>
